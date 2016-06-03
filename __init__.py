5fb30d841c33cb83b4f79b6fba42ce3a53187cee
tsterbak <sterbak-it@outlook.com>
2016-06-03 12:03:42 +0200

    first commit
    add python files

---
+++ __init__.py
@@ -0,0 +1,9 @@
+"""
+Includes RegularizedGreedyForestClassifier and RegularizedGreedyForestRegressor
+"""
+
+from .rgf import RegularizedGreedyForestClassifier
+from .rgf import RegularizedGreedyForestRegressor
+
+__all__ = ["RegularizedGreedyForestClassifier",
+           "RegularizedGreedyForestRegressor"]
